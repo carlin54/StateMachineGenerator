8c5cae7dbdc05d774c8e2548be107c1e6c6a2885
Carl Connell <carlin.connell@gmail.com>
2022-02-12 22:13:37 -0800

    added parameters, changed to take output directory, name and dpi

--- StateMachineGenerator.py
+++ StateMachineGenerator.py
@@ -64,8 +64,8 @@ def parse_event(current_state, line):
     return current_state, event, next_state
 
 
-def generate_state_machine(name, file, output_file):
-    sm_file = open(file, "r")
+def generate_state_machine(name, input_file, output_dir, dpi):
+    state_machine_file = open(input_file, "r")
 
     dict_states_to_events = dict()
     dict_states_to_superstates = dict()
@@ -74,7 +74,7 @@ def generate_state_machine(name, file, output_file):
     set_states = set()
     set_super_states = set()
     current_state = None
-    for line in sm_file:
+    for line in state_machine_file:
         if is_comment(line):
             continue
 
@@ -105,26 +105,19 @@ def generate_state_machine(name, file, output_file):
     graph = \
         "digraph finite_state_machine {\n" \
         "rankdir=LR;\n" \
-        "graph [ dpi = 700 ];\n" \
+        "graph [ dpi = " + dpi + " ];\n" \
         "size=\"24,24\";" \
 
     for state in set_states:
-        if state == "Unused":
-            continue
         graph += "\nnode [shape = circle, label=\"" + state + "\"] \"" + state + "_State\";"
 
     for event in set_events:
-        if event == "Unused":
-            continue
         if event in set_states:
             continue
         graph += "\nnode [shape = box, label=\"" + event + "\"] \"" + event + "_Event\";"
 
     edges = set()
     for state in dict_states_to_superstates:
-        if state == "Unused":
-            continue
-
         super_states = dict_states_to_superstates[state]
         for super_state in super_states:
             edge = (state, super_state)
@@ -133,16 +126,12 @@ def generate_state_machine(name, file, output_file):
                 graph += "\nedge [dir=\"both\"] \"" + state + "_State\" -> \"" + super_state + "_State\";"
 
     for start_state in dict_states_to_events:
-        if start_state == "Unused":
-            continue
         for event, next_state in dict_states_to_events[start_state]:
             edge = (start_state, event, next_state)
             if edge not in edges:
                 edges.add(edge)
                 if start_state == event == next_state:
                     graph += "\nedge [dir=\"forward\"] \"" + start_state + "_State\" -> \"" + start_state + "_State\";"
-                    #graph += "\nedge [dir=\"forward\"] \"" + start_state + "_State\" -> \"" + event + "_Event\";"
-                    #graph += "\nedge [dir=\"forward\"] \"" + event + "_Event\" -> \"" + next_state + "_State\";"
                 elif start_state == next_state and start_state != event:
                     graph += "\nedge [dir=\"both\"] \"" + start_state + "_State\" -> \"" + event + "_Event\";"
                 else:
@@ -154,52 +143,93 @@ def generate_state_machine(name, file, output_file):
     graph += "\nlabel=\"" + name + " State Machine Diagram - Generated on " + str(now)[0:19] + "\";"
     graph += "\n}"
 
-    filename = output_file + name + ".gv"
-    if verbose:
-        print("Writing to: " + filename)
-    s = Source(graph, filename=filename, format="png")
-    s.render()
+    s = Source(graph)
+    s.render(outfile=output_dir + name + ".png", overwrite_source=True,format='png')
 
+def usage():
+    print("-h, --help")
+    print("\tdisplays this text, then exits.")
+    print("-i, --input-file")
+    print("\tthe input libero state machine file to generate. A diagram and a dot file will be generated from the input file.")
+    print("-o, --output-file")
+    print("\tthe directory to output the dot and png files.")
+    print("-n, --name")
+    print("\tsets the name for the output dot and png files.")
+    print("-d, --dpi")
+    print("\tsets the dpi parameter for the generated image. Larger state machines may require a larger dpi.")
 
-def main(argv):
+def main():
     argv = sys.argv
     global verbose
+   
     verbose = True
     input_file = None
-    output_file = None
+    output_dir = None
+    name = None
+    dpi = None
+
     try:
-        opts, args = getopt.getopt(argv[1:],"i:o:",["input-file=","output-file="])
+        opts, args = getopt.getopt(argv[1:],"i:o:n:d:",["input-file=","output-dir=","name=","dpi="])
     except getopt.GetoptError:
-        print('GenerateStateMachineDiagram.py -i <input file> -o <output file>')
-        sys.exit(2)
+        usage()
+        sys.exit(1)
     for opt, arg in opts:
         if opt == '-h':
-            print('GenerateStateMachineDiagram.py -i <input file> -o <output file>')
+            usage()
             sys.exit()
         elif opt in ("-i", "--input-file"):
             input_file = arg
-        elif opt in ("-o", "--output-file"):
-            output_file = arg
+        elif opt in ("-o", "--output-dir"):
+            output_dir = arg
+        elif opt in ("-n", "--name"):
+            name = arg
+        elif opt in ("-d", "--dpi"):
+            dpi = arg
+        else:
+            usage()
+            sys.exit(2)
 
-    input_file = input_file if input_file[len(input_file)-1] == "/" else input_file + "/"
-    output_file = output_file if output_file[len(output_file) - 1] == "/" else output_file + "/"
-    print('Input directory is ', input_file)
-    print('Output directory is ', output_file)
-    os.path.isfile(
+    if input_file is None:
+        print("Error: input file is mandatory.")
+        sys.exit(3)
+    
+    if os.path.isdir(input_file):
+        print("Error: input file is directory and not a file.")
+        sys.exit()
 
-    for root, directories, files in os.walk(input_file, topdown=False):
+    if not os.path.isfile(input_file):
+        print("Error: input file is not a file.")
+        sys.exit(4)
 
-        if "state.m" in files:
-            # root = root.replace("\\" , "/")
-            idx = root.rfind("/") + 1
-            sm_name = root[idx:]
-            sm_file = root + "/" + "state.m"
-            if verbose:
-                print("Generating graph for " + sm_name + " state using " + sm_file + " as source.")
-            generate_state_machine(sm_name, sm_file, output_file)
+    if output_dir is None:
+        output_dir = os.path.dirname(input_file)
+        output_dir = os.path.splitext(output_dir)[0]
+    
+    if not output_dir.endswith("/"):
+        output_dir += "/"
 
+    if not os.path.isdir(output_dir):
+        print("Error: output directory is not a directory.")
+        sys.exit(5)
 
+    if name is None:
+        name = os.path.basename(input_file)
+        name = os.path.splitext(name)[0]
+    
+    if dpi is None:
+        dpi = "700"
+    
+    if not dpi.isnumeric():
+        print("Error: dpi is not numeric.")
+        sys.exit(6)
+    
+    if int(dpi) < 1:
+        print("Error: dpi is less than 1.")
+        sys.exit(7)
+    
+    generate_state_machine(name, input_file, output_dir, dpi)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
